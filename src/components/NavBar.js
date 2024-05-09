3aebea35fe6241d191239a2e4515505b3e7fba48
NgureWangai <ngurewangai@gmail.com>
2024-05-09 21:24:31 +0300

    complete task

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,12 +1,24 @@
+import React from "react";
 import { NavLink } from "react-router-dom";
 import "./NavBar.css";
 
 function NavBar() {
   return (
     <nav className="navbar">
-    
+      <NavLink to="/" className="nav-link" activeclassname="active">
+        Home
+      </NavLink>
+      <NavLink to="/directors" className="nav-link" activeclassname="active">
+        Directors
+      </NavLink>
+      <NavLink to="/actors" className="nav-link" activeclassname="active">
+        Actors
+      </NavLink>
     </nav>
     );
 };
 
 export default NavBar;
+
+
+
